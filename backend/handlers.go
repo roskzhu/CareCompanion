5274bfe46926df420d55992b591159462edf3ba2
roskzhu <roskenzhu@gmail.com>
2023-11-20 21:21:11 -0500

    switch speech to text to google

--- backend/handlers.go
+++ backend/handlers.go
@@ -2,9 +2,21 @@
 package main
 
 import (
-    "fmt"
+    "bytes"
+	"context"
+	"encoding/base64"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+
     "github.com/gin-gonic/gin"
     "github.com/cohere-ai/cohere-go"
+
+    "cloud.google.com/go/speech/apiv1"
+	"google.golang.org/api/option"
+	speechpb "google.golang.org/genproto/googleapis/cloud/speech/v1"
 )
 
 func ReceiveTranscriptHandler(c *gin.Context) {
@@ -33,4 +45,84 @@ func ReceiveTranscriptHandler(c *gin.Context) {
     }
 
     c.JSON(200, gin.H{"message": "Transcript received successfully", "response": response})
+}
+
+
+// Function to transcribe speech using Google Cloud Speech-to-Text API
+func transcribeSpeech(audioContent []byte) (string, error) {
+	ctx := context.Background()
+
+	// Replace 'your-google-cloud-credentials.json' with the path to your Google Cloud API key JSON file.
+	client, err := speech.NewClient(ctx, option.WithAPIKey(gcpAPIKey))
+	if err != nil {
+		return "", fmt.Errorf("failed to create Speech client: %v", err)
+	}
+	defer client.Close()
+
+	// Create a SpeechRecognition request
+	req := &speechpb.RecognizeRequest{
+		Config: &speechpb.RecognitionConfig{
+			Encoding:        speechpb.RecognitionConfig_LINEAR16,
+			SampleRateHertz: 16000,
+			LanguageCode:    "en-US",
+		},
+		Audio: &speechpb.RecognitionAudio{
+			AudioSource: &speechpb.RecognitionAudio_Content{Content: audioContent},
+		},
+	}
+
+	// Send the request to the Speech-to-Text API
+	resp, err := client.Recognize(ctx, req)
+	if err != nil {
+		return "", fmt.Errorf("failed to recognize speech: %v", err)
+	}
+
+	// Check if there are any results
+	if len(resp.Results) == 0 {
+		return "", fmt.Errorf("no speech recognition results found")
+	}
+
+	// Extract and join the transcript from the results
+	transcript := resp.Results[0].Alternatives[0].Transcript
+	return transcript, nil
+}
+
+// Handler function for processing audio data from the frontend
+func handleAudioRequest(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+    // Read the Google Cloud API key from an environment variable
+	apiKey := os.Getenv("GOOGLE_CLOUD_API_KEY")
+	if apiKey == "" {
+		http.Error(w, "Google Cloud API key not set", http.StatusInternalServerError)
+		return
+	}
+
+	// Read the audio data from the request body
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Error reading request body", http.StatusInternalServerError)
+		return
+	}
+
+	// Decode base64-encoded audio data
+	audioData, err := base64.StdEncoding.DecodeString(string(body))
+	if err != nil {
+		http.Error(w, "Error decoding audio data", http.StatusBadRequest)
+		return
+	}
+
+	// Transcribe the speech using the Speech-to-Text API
+	transcription, err := transcribeSpeech(audioData)
+	if err != nil {
+		log.Printf("Speech transcription error: %v", err)
+		http.Error(w, "Error transcribing speech", http.StatusInternalServerError)
+		return
+	}
+
+	// Return the transcription to the frontend
+	fmt.Fprintf(w, "Transcription: %s", transcription)
 }
--- frontend/pages/chat.tsx
+++ frontend/pages/chat.tsx
@@ -1,15 +1,94 @@
 // pages/chat.tsx
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Layout from '../components/Layout';
 import ChatComponent from '../components/ChatComponent';
 
-const ChatPage = () => (
-  <Layout title="Chat Page | Next.js + TypeScript Example">
-    <div>
-      <h1>Chat Page</h1>
-      <ChatComponent />
-    </div>
-  </Layout>
-);
+const ChatPage = () => {
+  const [microphoneAccess, setMicrophoneAccess] = useState<boolean | null>(null);
+  const [speechRecognition, setSpeechRecognition] = useState<SpeechRecognition | null>(null);
+
+  useEffect(() => {
+    const requestMicrophoneAndSpeechRecognition = async () => {
+      try {
+        // Request microphone access
+        const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+        setMicrophoneAccess(true);
+
+        // Set up SpeechRecognition
+        const recognition = new SpeechRecognition();
+        recognition.continuous = true;
+        recognition.interimResults = true;
+
+        recognition.onstart = () => console.log('Speech recognition started.');
+        recognition.onerror = (event) => console.error('Speech recognition error:', event.error);
+        recognition.onresult = async (event) => {
+          const transcript = Array.from(event.results)
+            .map((result) => result[0].transcript)
+            .join('');
+          console.log('Speech recognition result:', transcript);
+
+          // Encode the transcript to base64
+          const audioData = btoa(transcript);
+
+          // Send a POST request to your backend
+          try {
+            const response = await fetch('http://your-backend/transcribe', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json', // or 'application/x-www-form-urlencoded', depending on your backend
+              },
+              body: JSON.stringify({ audioData }),
+            });
+
+            if (!response.ok) {
+              console.error('Failed to send transcription to backend:', response.statusText);
+            } else {
+              console.log('Transcription sent to backend successfully.');
+            }
+          } catch (error) {
+            console.error('Error sending transcription to backend:', error);
+          }
+        };
+
+        // Start speech recognition
+        recognition.start();
+
+        // Save the SpeechRecognition instance in state
+        setSpeechRecognition(recognition);
+
+        // Don't forget to stop the stream when you're done using it
+        stream.getTracks().forEach(track => track.stop());
+      } catch (error) {
+        console.error('Error accessing the microphone:', error);
+        setMicrophoneAccess(false);
+      }
+    };
+
+    // Call the function to request microphone access and set up speech recognition
+    requestMicrophoneAndSpeechRecognition();
+
+    // Clean up: stop speech recognition when the component unmounts
+    return () => {
+      if (speechRecognition) {
+        speechRecognition.stop();
+      }
+    };
+  }, [speechRecognition]);
+
+  return (
+    <Layout title="Chat Page">
+      <div>
+        <h1>Chat Page</h1>
+        {microphoneAccess === null ? (
+          <p>Requesting microphone access...</p>
+        ) : microphoneAccess ? (
+          <ChatComponent />
+        ) : (
+          <p>Microphone access denied or not available.</p>
+        )}
+      </div>
+    </Layout>
+  );
+};
 
 export default ChatPage;
